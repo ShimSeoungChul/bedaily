146c5068b158ca8509d329df6c11ed4c89b2fec0
Seungchul Shim <tlatmdcjf94@naver.com>
2026-07-02 08:13:48 +0900

    docs(posts): 엘라스틱서치 글 수정

--- components/mdx/mdx-components.tsx
+++ components/mdx/mdx-components.tsx
@@ -2,9 +2,14 @@ import type { MDXComponents } from 'mdx/types'
 import CodeBlock from './CodeBlock'
 import MDXImage from './MDXImage'
 import Figure from './Figure'
+import { TokenDemo, TokenInput, TokenRow, Chip } from './TokenDemo'
 
 export const mdxComponents: MDXComponents = {
   pre: CodeBlock as any,
   img: MDXImage as any,
   Figure: Figure as any,
+  TokenDemo: TokenDemo as any,
+  TokenInput: TokenInput as any,
+  TokenRow: TokenRow as any,
+  Chip: Chip as any,
 }
